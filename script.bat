be0483dbc0d464ac7d7bd8e48a97415b4e1fbe27
cyber1709 <reachgauravarora@outlook.com>
2024-03-12 14:53:00 +0530

    output file name changed

--- script.bat
+++ script.bat
@@ -3,30 +3,30 @@
 
 REM Write date and time to a text file
 echo Getting Date and time
-echo Date: %date% > output.txt
-echo Time: %time% >> output.txt
+echo Date: %date% > system_audit_info.txt
+echo Time: %time% >> system_audit_info.txt
 
 set num_users=0
 REM Get number of all users
 for /f "skip=4 delims=" %%i in ('net user ^| find /v "command completed successfully."') do set /a "num_users+=1"
 
 REM Write number of all users to the text file
-echo Number of all users: %num_users% >> output.txt
+echo Number of all users: %num_users% >> system_audit_info.txt
 
 REM Get name of logged in user
-echo Logged in user: %USERNAME% >> output.txt
+echo Logged in user: %USERNAME% >> system_audit_info.txt
 
 REM Get full username
 for /f "tokens=2 delims==\ " %%U in ('wmic useraccount where name="%USERNAME%" get fullname /value ^| find "="') do (
-    echo Full username: %%U >> output.txt
+    echo Full username: %%U >> system_audit_info.txt
 )
 
 REM Get hostname of the computer
-echo Hostname: %COMPUTERNAME% >> output.txt
+echo Hostname: %COMPUTERNAME% >> system_audit_info.txt
 
 REM Get OS name
 for /f "tokens=2 delims==" %%O in ('wmic os get Caption /value ^| find "="') do (
-    echo Operating System: %%O >> output.txt
+    echo Operating System: %%O >> system_audit_info.txt
 )
 
 REM Get OS install date
@@ -36,7 +36,7 @@ for /f "tokens=2 delims==" %%I in ('wmic os get InstallDate /value ^| find "="')
 
 REM Convert install date to a more readable format (YYYYMMDD)
 set "InstallDate=%InstallDate:~0,4%-%InstallDate:~4,2%-%InstallDate:~6,2% %InstallDate:~8,2%:%InstallDate:~10,2%:%InstallDate:~12,2%"
-echo OS Install Date: %InstallDate% >> output.txt
+echo OS Install Date: %InstallDate% >> system_audit_info.txt
 
 REM Check if we have administrative privileges
 >nul 2>&1 net session
@@ -53,25 +53,25 @@ for /F "tokens=2 delims=:" %%A in ('net accounts ^| findstr /C:"Minimum password
 
 REM Check if password complexity is enforced (Minimum 8 characters containing alphanumeric characters)
 if %MinPasswordLength% geq 8 (
-    echo Password complexity: Minimum 8 characters containing alphanumeric characters. >> output.txt
+    echo Password complexity: Minimum 8 characters containing alphanumeric characters. >> system_audit_info.txt
 ) else (
-    echo Password complexity: Not enforced. >> output.txt
+    echo Password complexity: Not enforced. >> system_audit_info.txt
 )
 
-REM Check if screen saver is enabled and write the result to output.txt
+REM Check if screen saver is enabled and write the result to system_audit_info.txt
 reg query "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveActive > nul 2>&1
 if %errorlevel% equ 0 (
-    echo Screen saver is enabled. >> output.txt
+    echo Screen saver is enabled. >> system_audit_info.txt
 ) else (
-    echo Screen saver is not enabled. >> output.txt
+    echo Screen saver is not enabled. >> system_audit_info.txt
 )
 
 REM Check if internet is connected by pinging a well-known website
 ping -n 1 google.com > nul 2>&1
 if %errorlevel% equ 0 (
-    echo Internet is connected. >> output.txt
+    echo Internet is connected. >> system_audit_info.txt
 ) else (
-    echo Internet is not connected. >> output.txt
+    echo Internet is not connected. >> system_audit_info.txt
 )
 
 REM Get public IP address
@@ -84,17 +84,17 @@ for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /C:"IPv4 Address"') do (
     echo Local IP address: %%a >> ip_allocated.txt
 )
 
-echo Screen saver status checked, internet connection status, public and local IP addresses saved to output.txt.
+echo Screen saver status checked, internet connection status, public and local IP addresses saved to system_audit_info.txt.
 
 REM Check if we have administrative privileges
 >nul 2>&1 net session
 if %errorlevel% equ 0 (
-    echo User privilege: Administrator >> output.txt
+    echo User privilege: Administrator >> system_audit_info.txt
 ) else (
-    echo User privilege: Limited >> output.txt
+    echo User privilege: Limited >> system_audit_info.txt
 )
 
-echo User privileges checked and saved to output.txt.
+echo User privileges checked and saved to system_audit_info.txt.
 
 
 REM Get MAC address
@@ -105,9 +105,9 @@ for /f "tokens=2 delims= " %%m in ('getmac /fo table /nh') do (
 REM Check if the guest account is enabled
 net user guest > nul 2>&1
 if %errorlevel% equ 0 (
-    echo Guest account is enabled. >> output.txt
+    echo Guest account is enabled. >> system_audit_info.txt
 ) else (
-    echo Guest account is not enabled. >> output.txt
+    echo Guest account is not enabled. >> system_audit_info.txt
 )
 
 REM Check if Remote Desktop is enabled
@@ -115,12 +115,12 @@ reg query "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server" 
 if %errorlevel% equ 0 (
     reg query "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections | findstr "REG_DWORD.*0x0" > nul 2>&1
     if %errorlevel% equ 0 (
-        echo Remote Desktop is enabled. >> output.txt
+        echo Remote Desktop is enabled. >> system_audit_info.txt
     ) else (
-        echo Remote Desktop is not enabled. >> output.txt
+        echo Remote Desktop is not enabled. >> system_audit_info.txt
     )
 ) else (
-    echo Unable to determine Remote Desktop status. >> output.txt
+    echo Unable to determine Remote Desktop status. >> system_audit_info.txt
 )
 
 REM Check if autoplay is enabled
@@ -128,12 +128,12 @@ reg query "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies
 if %errorlevel% equ 0 (
     reg query "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDriveTypeAutoRun | findstr "0x0" > nul 2>&1
     if %errorlevel% equ 0 (
-        echo Autoplay is enabled. >> output.txt
+        echo Autoplay is enabled. >> system_audit_info.txt
     ) else (
-        echo Autoplay is not enabled. >> output.txt
+        echo Autoplay is not enabled. >> system_audit_info.txt
     )
 ) else (
-    echo Unable to determine Autoplay status. >> output.txt
+    echo Unable to determine Autoplay status. >> system_audit_info.txt
 )
 
 
@@ -142,12 +142,12 @@ reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Par
 if %errorlevel% equ 0 (
     reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile" /v EnableFirewall | findstr "REG_DWORD.*0x1" > nul 2>&1
     if %errorlevel% equ 0 (
-        echo Windows Firewall is enabled. >> output.txt
+        echo Windows Firewall is enabled. >> system_audit_info.txt
     ) else (
-        echo Windows Firewall is disabled. >> output.txt
+        echo Windows Firewall is disabled. >> system_audit_info.txt
     )
 ) else (
-    echo Unable to determine Windows Firewall status. >> output.txt
+    echo Unable to determine Windows Firewall status. >> system_audit_info.txt
 )
 
 
@@ -156,9 +156,9 @@ REM Check for installed antivirus software and display its name
 echo Checking for antivirus software...
 wmic /namespace:\\root\SecurityCenter2 path antivirusproduct get /value | findstr /i /c:"displayName" > nul 2>&1
 if %errorlevel% equ 0 (
-    echo Antivirus software is present. >> output.txt
+    echo Antivirus software is present. >> system_audit_info.txt
 ) else (
-    echo Antivirus software is not present. >> output.txt
+    echo Antivirus software is not present. >> system_audit_info.txt
 )
 
 wmic /namespace:\\root\SecurityCenter2 path antivirusproduct get displayName /value | findstr /i "displayName" > nul 2>&1
@@ -173,12 +173,12 @@ reg query "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\
 if %errorlevel% equ 0 (
     reg query "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate | findstr "0x0" > nul 2>&1
     if %errorlevel% equ 0 (
-        echo Automatic Updates are enabled. >> output.txt
+        echo Automatic Updates are enabled. >> system_audit_info.txt
     ) else (
-        echo Automatic Updates are not enabled. >> output.txt
+        echo Automatic Updates are not enabled. >> system_audit_info.txt
     )
 ) else (
-    echo Unable to determine Automatic Updates status. >> output.txt
+    echo Unable to determine Automatic Updates status. >> system_audit_info.txt
 )
 
 
