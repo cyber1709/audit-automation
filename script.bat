867690de0c4f2ae1191b5ab1a82e327e28bba780
cyber1709 <reachgauravarora@outlook.com>
2024-03-12 12:29:01 +0530

    user priviledge checked

--- script.bat
+++ script.bat
@@ -16,10 +16,6 @@ echo Number of all users: %num_users% >> output.txt
 REM Get name of logged in user
 echo Logged in user: %USERNAME% >> output.txt
 
-echo Date, time, number of all users, and logged in user saved to output.txt
-
-
-
 REM Check if we have administrative privileges
 >nul 2>&1 net session
 if %errorlevel% neq 0 (
@@ -29,28 +25,19 @@ if %errorlevel% neq 0 (
 )
 
 REM Query Security Policy for password complexity setting
-for /F "tokens=3" %%A in ('net accounts ^| findstr /C:"Minimum password length"') do (
-    set "PasswordComplexity=%%A"
+for /F "tokens=2 delims=:" %%A in ('net accounts ^| findstr /C:"Minimum password length"') do (
+    set "MinPasswordLength=%%A"
 )
 
-REM Check if password complexity is enforced
-if "%PasswordComplexity%" equ "0" (
-    echo Windows password protection is not enabled.
+REM Check if password complexity is enforced (Minimum 8 characters containing alphanumeric characters)
+if %MinPasswordLength% geq 8 (
+    echo Password complexity: Minimum 8 characters containing alphanumeric characters. >> output.txt
 ) else (
-    echo Windows password protection is enabled.
-) >> output.txt
-
-
-REM Check if we have administrative privileges
->nul 2>&1 net session
-if %errorlevel% neq 0 (
-    echo Error: Administrative privileges required. Please run this script as an administrator.
-    pause
-    exit /b
+    echo Password complexity: Not enforced. >> output.txt
 )
 
 REM Check if screen saver is enabled and write the result to output.txt
-reg query "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveActive
+reg query "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveActive > nul 2>&1
 if %errorlevel% equ 0 (
     echo Screen saver is enabled. >> output.txt
 ) else (
@@ -58,3 +45,13 @@ if %errorlevel% equ 0 (
 )
 
 echo Screen saver status checked and saved to output.txt.
+
+REM Check if we have administrative privileges
+>nul 2>&1 net session
+if %errorlevel% equ 0 (
+    echo User privilege: Administrator >> output.txt
+) else (
+    echo User privilege: Limited >> output.txt
+)
+
+echo User privileges checked and saved to output.txt.
